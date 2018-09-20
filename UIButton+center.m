40318aafd0fe68e36e0abf764b37a0bf7e07d380
lcccocoa <luoqu520@gmail.com>
2018-09-20 17:44:12 +0800

    Create UIButton+center.m

---
+++ UIButton+center.m
@@ -0,0 +1,23 @@
+#import "UIButton+center.h"
+
+@implementation UIButton (center)
+
+-(void)contentCenterWithSpace:(CGFloat)space {
+    
+    
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    CGSize textSize = [self.titleLabel.text sizeWithAttributes:@{
+                                                                 NSFontAttributeName: self.titleLabel.font,
+                                                                 }];
+    
+    CGSize frameSize = CGSizeMake(ceilf(textSize.width), ceilf(textSize.height));
+    if (titleSize.width + 0.5 < frameSize.width) {
+        titleSize.width = frameSize.width;
+    }
+    CGFloat totalHeight = (imageSize.height + titleSize.height + space);
+    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), 0.0, 0.0, - titleSize.width);
+    self.titleEdgeInsets = UIEdgeInsetsMake(0, - imageSize.width, - (totalHeight - titleSize.height), 0);
+}
+
+@end
